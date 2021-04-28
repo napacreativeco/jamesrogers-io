d12b33b5d3b8c558d79a991a6cc8908be8ed6751
James Rogers <hello@napacreativeco.com>
2021-04-27 17:47:08 -0700

    added a preloader

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,4 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
@@ -21,6 +14,21 @@ const MainContent = styled.div`
     margin: 0px auto 0px auto;
   }
 `;
+const Preloader = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: #fff;
+  z-index: 9999;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  font-family: var(--title);
+`;
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -32,13 +40,26 @@ const Layout = ({ children }) => {
     }
   `)
 
+  useEffect(() => {
+    const preLoader = document.querySelector(".preloader");
+
+    setTimeout(() => {
+      preLoader.style.display = "none"
+    }, 2500);
+
+  });
+
   return (
     <>
       <Headroom>
        <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       </Headroom>
 
-      
+      <Preloader className="preloader">
+        <p>Just a sec.</p>
+        <img src="/7zxq.gif" alt="Just a sec..." style={{ width: '250px'}} />
+      </Preloader>
+
       <MainContent>
         {children}
       </MainContent>
